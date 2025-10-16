b3a5f81e196b0245b5db7cb04e4a223f9f5a3af6
LuisM <muislarques@gmail.com>
2025-10-17 00:10:39 +0100

    codealong continuation. store function in variable, use function as data type, write lambda  expressions and use repeat

--- src/functiontypesandlambdaexpressions/codeAlong.kt
+++ src/functiontypesandlambdaexpressions/codeAlong.kt
@@ -1,10 +1,32 @@
 package functiontypesandlambdaexpressions
 
 fun main() {
-    val trickFunction = trick
+    val treatFunction = trickOrTreat(false) {
+        "$it quarters"
+    }
+    val trickFunction = trickOrTreat(true, null)
+
+    repeat(4) {
+        treatFunction()
+    }
     trickFunction()
+}
+
+fun trickOrTreat(isTrick: Boolean, extraTreat: ((Int) -> String)?): () -> Unit {
+    if (isTrick) {
+        return trick
+    } else {
+        if(extraTreat != null) {
+            println(extraTreat(5))
+        }
+        return treat
+    }
 }
 
 val trick = {
     println("No treats!")
+}
+
+val treat: () -> Unit = {
+    println("have a treat!")
 }
