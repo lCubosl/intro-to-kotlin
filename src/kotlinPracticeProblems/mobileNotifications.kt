702b13f82ec260a2b9a6c81475f34e798171fab7
LuisM <muislarques@gmail.com>
2025-10-18 01:04:41 +0100

    mobileNotifications

---
+++ src/kotlinPracticeProblems/mobileNotifications.kt
@@ -0,0 +1,17 @@
+package kotlinPracticeProblems
+
+fun main() {
+    val morningNotifications = 51
+    val eveningNotifications = 135
+
+    printNotificationSummary(morningNotifications)
+    printNotificationSummary(eveningNotifications)
+}
+
+fun printNotificationSummary(numberOfMessages: Int) {
+    if (numberOfMessages < 99) {
+        println("You have $numberOfMessages notifications.")
+    } else {
+        println("You have 99+ notifications")
+    }
+}
