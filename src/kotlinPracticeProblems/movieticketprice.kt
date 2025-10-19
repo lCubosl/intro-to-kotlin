8f58a7394bd6357b7955a434f45fb412088a551c
LuisM <muislarques@gmail.com>
2025-10-19 20:45:18 +0100

    movie ticket price no solution yet

--- src/kotlinPracticeProblems/movieticketprice.kt
+++ src/kotlinPracticeProblems/movieticketprice.kt
@@ -13,5 +13,10 @@ fun main() {
 }
 
 fun ticketPrice(age: Int, isMonday: Boolean): Int {
-    // code
+    return  when(age) {
+        in 0..12 -> 15
+        in 13..60 -> if (isMonday) 25 else 30
+        in 61..100 -> 20
+        else -> -1
+    }
 }
